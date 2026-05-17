485f3a15b828ab53efaecfd36831a09ef853492b
Vic Garcia <vic.garcia@outlook.com>
2026-05-16 21:41:32 -0400

    rename identifier to vicgarcia.pinboard.in-bookmarker

--- bookmarker for pinboard/ViewController.swift
+++ bookmarker for pinboard/ViewController.swift
@@ -98,7 +98,7 @@ class ViewController: NSViewController {
     }
     
     @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
-        SFSafariApplication.showPreferencesForExtension(withIdentifier: "vicg4rcia.pinboard.in-bookmarker.extension") { error in
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: "vicgarcia.pinboard.in-bookmarker.extension") { error in
             if let _ = error {
                 // Insert code to inform the user that something went wrong.
 
